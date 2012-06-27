b8c1b54b870c4fecc87d4daae44b34c262b0e6f8
Jeremy Mikola <jmikola@gmail.com>
2012-06-27 17:43:21 -0400

    Use Composer autoloader in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,9 @@
 <?php
 
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
+if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 
-require_once $file;
+$loader = require_once $file;
 
-use Doctrine\Common\ClassLoader;
-
-$classLoader = new ClassLoader('Doctrine\MongoDB\Tests', __DIR__ . '/../tests');
-$classLoader->register();
+$loader->add('Doctrine\MongoDB\Tests', __DIR__ . '/../tests');
