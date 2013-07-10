8b6f6e171a63e00957080ba3948e7cc148f22de6
Jeremy Mikola <jmikola@gmail.com>
2013-07-10 15:42:18 -0400

    Clean up docs in various auxiliary classes

--- lib/Doctrine/MongoDB/Events.php
+++ lib/Doctrine/MongoDB/Events.php
@@ -1,7 +1,5 @@
 <?php
 /*
- *  $Id$
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -22,14 +20,12 @@
 namespace Doctrine\MongoDB;
 
 /**
- * Container for all Doctrine\MongoDB events.
+ * Container class for all Doctrine MongoDB events.
  *
  * This class cannot be instantiated.
  *
- * @license     http://www.opensource.org/licenses/mit-license.php MIT
- * @link        www.doctrine-project.com
- * @since       1.0
- * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @since  1.0
+ * @author Jonathan H. Wage <jonwage@gmail.com>
  */
 final class Events
 {
