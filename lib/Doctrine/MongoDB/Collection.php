30aa713f39141e986c08e7e14aaf803bb7a0bdd5
Jeremy Mikola <jmikola@gmail.com>
2014-04-09 23:56:49 -0700

    Collection::ensureIndex() should convert write options (fixes #168)

--- lib/Doctrine/MongoDB/Collection.php
+++ lib/Doctrine/MongoDB/Collection.php
@@ -485,6 +485,7 @@ class Collection
 
     public function ensureIndex(array $keys, array $options = array())
     {
+        $options = isset($options['safe']) ? $this->convertWriteConcern($options) : $options;
         return $this->getMongoCollection()->ensureIndex($keys, $options);
     }
 
