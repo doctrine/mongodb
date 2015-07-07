93aa0c92195ee52fb537610742da40b7d0765e73
Andreas Braun <a.braun@voycer.com>
2015-07-07 20:22:56 +0200

    Remove unsupported methods from match stage

--- lib/Doctrine/MongoDB/Aggregation/Stage/Match.php
+++ lib/Doctrine/MongoDB/Aggregation/Stage/Match.php
@@ -108,30 +108,6 @@ class Match extends Stage
     }
 
     /**
-     * Append one or more values to the current array field only if they do not
-     * already exist in the array.
-     *
-     * If the field does not exist, it will be set to an array containing the
-     * unique value(s) in the argument. If the field is not an array, the query
-     * will yield an error.
-     *
-     * Multiple values may be specified by provided an Expr object and using
-     * {@link Expr::each()}.
-     *
-     * @see Expr::addToSet()
-     * @see http://docs.mongodb.org/manual/reference/operator/addToSet/
-     * @see http://docs.mongodb.org/manual/reference/operator/each/
-     * @param mixed|Expr $valueOrExpression
-     * @return $this
-     */
-    public function addToSet($valueOrExpression)
-    {
-        $this->query->addToSet($valueOrExpression);
-
-        return $this;
-    }
-
-    /**
      * Specify $all criteria for the current field.
      *
      * @see Expr::all()
@@ -614,21 +590,6 @@ class Match extends Stage
     public function type($type)
     {
         $this->query->type($type);
-
-        return $this;
-    }
-
-    /**
-     * Specify a JavaScript expression to use for matching documents.
-     *
-     * @see Expr::where()
-     * @see http://docs.mongodb.org/manual/reference/operator/where/
-     * @param string|\MongoCode $javascript
-     * @return $this
-     */
-    public function where($javascript)
-    {
-        $this->query->where($javascript);
 
         return $this;
     }
