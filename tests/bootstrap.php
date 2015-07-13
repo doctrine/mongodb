18cd3b46ad4517715420e46cb5c20bccb9feffe8
Andreas Braun <a.braun@voycer.com>
2015-07-13 21:15:04 +0200

    Revert change to test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 
-$loader = require $file;
+$loader = require_once $file;
 
 $loader->add('Doctrine\MongoDB\Tests', __DIR__ . '/../tests');
